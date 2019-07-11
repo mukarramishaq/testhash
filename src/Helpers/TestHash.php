51111a8f9a1f2f038a66a061637837be47bb8c6c
Mukarram Ishaq <mukarram.ishaq@rolustech.net>
2019-07-11 22:02:02 +0500

    update the documentation

--- src/Helpers/TestHash.php
+++ src/Helpers/TestHash.php
@@ -5,15 +5,15 @@ namespace Mukarramishaq\TestHash\Helpers;
 class TestHash {
     /**
      * hash function creates a hash of given string in the parameters.
-     *
+     * this function uses the modulo hashing algorithm (h(K) = K mod n) with a slight variance
      * @param [type] $str
-     * @param integer $size [optional] hash list size
-     * @param string $characters [optional] characters string to which the hash key should be mapped
-     * @return string hashed value
+     * @param integer $size [optional] hash length size by default it is 50
+     * @param string $characters [optional] characters string to which the hash key should be mapped. by default it is '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ' 
+     * @return string
      * 
      * @static
      */
-    public static function hash($str, $size = 5, $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    public static function hash($str, $size = 50, $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
     {
         $hashArray = array();
         $hash = '';
